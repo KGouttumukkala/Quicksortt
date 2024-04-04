203c7961088dbe93f202db479cedb6f1f97c82ac
KGouttumukkala <Goutt8@vt.edu>
2024-04-03 20:08:53 -0400

    a

--- Buffer.java
+++ Buffer.java
@@ -1,4 +1,39 @@
+import java.io.*;
 
 public class Buffer {
-
+    private byte[] data;
+    private boolean isDirty;
+    private int bufferSize;
+    
+    public Buffer(int size) {
+        bufferSize = size;
+        data = new byte[bufferSize];
+        isDirty = false;
+    }
+    
+    public void readFromFile(FileInputStream fis, int blockSize) throws IOException {
+        fis.read(data, 0, blockSize);
+    }
+    
+    public void writeToFile(FileOutputStream fos) throws IOException {
+        fos.write(data);
+        isDirty = false;
+    }
+    
+    public byte[] getData() {
+        return data;
+    }
+    
+    public void setData(byte[] newData) {
+        data = newData;
+        isDirty = true;
+    }
+    
+    public boolean isDirty() {
+        return isDirty;
+    }
+    
+    public int getBufferSize() {
+        return bufferSize;
+    }
 }
