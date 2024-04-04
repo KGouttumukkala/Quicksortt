22a7831d0d24d28d5c362ace5cabfaf0d2accbce
KGouttumukkala <Goutt8@vt.edu>
2024-04-03 20:22:42 -0400

    a

--- BufferPool.java
+++ BufferPool.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class BufferPool {
+    private List<Buffer> buffers;
+    private int capacity;
+    private int bufferSize;
 
+    public BufferPool(int cap, int buffSize) {
+        capacity = cap;
+        buffers = new ArrayList<>(capacity);
+        bufferSize = buffSize;
+        initializeBuffers();
+    }
+
+
+    private void initializeBuffers() {
+        for (int i = 0; i < capacity; i++) {
+            buffers.add(new Buffer(bufferSize));
+        }
+    }
+
+
+    public Buffer getBuffer() {
+        Buffer leastRecentlyUsed = buffers.get(0);
+        for (Buffer buffer: buffers) {
+            if (buffer.getLastUsedTime() < leastRecentlyUsed.getLastUsedTime()) {
+                leastRecentlyUsed = buffer;
+            }
+        }
+        
+        leastRecentlyUsed.setLastUsedTime(System.currentTimeMillis());
+        
+        return leastRecentlyUsed;
+    }
+
+
+    public void releaseBuffer(Buffer buffer) {
+        buffer.reset();
+    }
 }
